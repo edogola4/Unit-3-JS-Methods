c2c3f4c90bc3034f290c63af47827824d9c2b82e
edogola4 <edogola4@gmail.com>
2024-11-05 18:09:49 +0300

    Js-Methods

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Your code here
+
+// define the cat class
+class Cat {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  // Define the Dog class
+  class Dog {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+  // Define the Bird class
+  class Bird {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      // Conditional output based on the sex of the bird
+      if (this.sex === "male") {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+  
+  // Example usage:
+  const cat = new Cat("Sudhe", "female");
+  console.log(cat.speak()); 
+  
+  const dog = new Dog("Sadam", "male");
+  console.log(dog.speak()); // "Buddy says woof!"
+  
+  const maleBird = new Bird("Polly", "male");
+  console.log(maleBird.speak()); // "It's me! Polly, the parrot!"
+  
+  const femaleBird = new Bird("Tweety", "female");
+  console.log(femaleBird.speak()); // "Tweety says squawk!"
+  
